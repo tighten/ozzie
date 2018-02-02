a518c09c102818b8ad7614f75b71b3a1b12bd4ac
Shawn Jones <shawn.greg.jones@gmail.com>
2018-02-02 12:32:28 -0600

    initialize tailwind preset and tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,13 @@
 let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +21,51 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .postCss('resources/assets/css/main.css', 'public/css', [
+        tailwindcss('tailwind.js'),
+   ]);
+
+// If you want to use LESS for your preprocessing
+// mix.less('resources/assets/less/main.less', 'public/css')
+//   .options({
+//     postCss: [
+//       tailwindcss('./tailwind.js'),
+//     ]
+//   })
+
+// If you want to use SASS for preprocessing
+// mix.sass('resources/assets/sass/app.scss', 'public/css')
+//    .options({
+//       processCssUrls: false,
+//       postCss: [ tailwindcss('tailwind.js') ],
+//    });
+
+// PurgeCSS is a utility that parses your application view files, identifies which
+// utility classes are actually being used, removing unused classes. This means
+// your final CSS will be much smaller, containing just the styles you need.
+//
+// Only run PurgeCSS in production builds keeping your development builds fast,
+// whilst still leaving you with a full set of utilities during development.
+// if (mix.inProduction()) {
+//   mix.webpackConfig({
+//     plugins: [
+//       new PurgecssPlugin({
+//
+//         // Specify the locations of any files you want to scan for class names.
+//         paths: glob.sync([
+//           path.join(__dirname, "resources/views/**/*.blade.php"),
+//           path.join(__dirname, "resources/assets/js/**/*.vue")
+//         ]),
+//         extractors: [
+//           {
+//             extractor: TailwindExtractor,
+//
+//             // Specify the file extensions to include when scanning for class names.
+//             extensions: ["html", "js", "php", "vue"]
+//           }
+//         ]
+//       })
+//     ]
+//   });
+// }
+
