1d6a360cef2e5da84dbbc92c5df71611d91c13ed
Anthony Terrell <ant@Anthonys-MacBook-Pro.local>
2018-04-27 21:45:00 -0500

    Update DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,31 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Project;
-use Carbon\Carbon;
-use GrahamCampbell\GitHub\Facades\GitHub;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
     public function show()
     {
-        try {
-            $projects = Cache::remember('projects', 60, function () {
-                $projects = collect(json_decode(file_get_contents(base_path('projects.json'))))->sortBy('name');
+        $projects = Cache::remember('projects', 60, function () {
+            $projects = collect(json_decode(file_get_contents(base_path('projects.json'))))->sortBy('name');
 
-                return $projects->map(function ($project) {
-                    return new Project($project->namespace, $project->name, $project->maintainers);
-                });
+            return $projects->map(function ($project) {
+                return new Project($project->namespace, $project->name, $project->maintainers);
             });
+        });
 
-            return view('dashboard')->with('projects', $projects);
-        } catch (\Exception $exception) {
-            Log::error($exception->getMessage());
-
-            abort(404);
-        }
+        return view('dashboard')->with('projects', $projects);
     }
 }
