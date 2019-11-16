d5f0389792b11b80b90eee0a319821baf45cd509
Sara Bine <sara@sarabine.com>
2019-11-15 16:53:18 -0800

    Upgrade to latest Laravel 6.x

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
-let mix = require('laravel-mix');
-let tailwindcss = require('tailwindcss');
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 mix.postCss('resources/css/main.css', 'public/css', [
-        tailwindcss('tailwind.js'),
-   ]);
+    tailwindcss('tailwind.js'),
+]);
