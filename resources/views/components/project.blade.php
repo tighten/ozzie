f43f8be97b1563bc44974a30e5d80079e7b1b4e6
Jon Sugar <jonathansugar@mac.com>
2020-04-29 11:51:31 +0100

    feat: Add project statistics below project name and maintainers.
    Add Debt Score and counts for: Old Prs, Old Issues, Prs and Issues below the project name and maintainers.

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,25 +1,60 @@
-<div class="mt-16">
-    <section class="flex justify-between items-center py-6 border-b-2 border-clouds"
-             id="project-{{ $project->namespace }}-{{ $project->name }}">
-        <h2 class="w-1/2 text-2xl text-black font-semibold tracking-wide">
-            <a href="{{ $project->url() }}" aria-label="Launch">
-                {{ $project->namespace }} | {{ $project->name }}
-                @include('svg.launch')
-            </a>
-        </h2>
+<section class="mt-16">
 
-        <p class="w-1/2 text-right text-black-lightest">
-            Maintained by
+    {{--Project Header--}}
+    <div class="flex flex-col">
 
-            @foreach ($project->maintainers as $maintainer)
-                <a class="text-indigo no-underline" href="https://github.com/{{ $maintainer }}"
-                   target="_blank">{{ '@' . $maintainer }}</a>
-            @endforeach
-        </p>
-    </section>
+        {{--Name / Maintainers--}}
+        <div class="flex justify-between items-end"
+            id="project-{{ $project->namespace }}-{{ $project->name }}">
+            <h2 class="w-1/2 text-2xl text-black font-semibold tracking-wide">
+                <a href="{{ $project->url() }}" aria-label="Launch">
+                    {{ $project->namespace }} | {{ $project->name }}
+                    @include('svg.launch')
+                </a>
+            </h2>
 
+            <p class="w-1/2 text-right text-black-lightest">
+                Maintained by
 
-    <section class="flex items-start border-b border-clouds">
+                @foreach ($project->maintainers as $maintainer)
+                    <a class="text-indigo no-underline" href="https://github.com/{{ $maintainer }}"
+                        target="_blank">{{ '@' . $maintainer }}</a>
+                @endforeach
+            </p>
+        </div>
+
+        {{--Debt stats--}}
+        <div class="mt-4 rounded-lg shadow">
+            <ul class="bg-grey-blue-light flex p-4 rounded-t-lg border-grey border-b-2">
+                <li class="w-1/5 text-grey-darkest font-bold uppercase text-xs leading-none tracking-wide">Debt Score</li>
+
+                <li class="w-1/5 text-grey-darkest font-bold uppercase text-xs leading-none tracking-wide">Old Prs</li>
+
+                <li class="w-1/5 text-grey-darkest font-bold uppercase text-xs leading-none tracking-wide">Old Issues</li>
+
+                <li class="w-1/5 text-grey-darkest font-bold uppercase text-xs leading-none tracking-wide">Prs</li>
+
+                <li class="w-1/5 text-grey-darkest font-bold uppercase text-xs leading-none tracking-wide">Issues</li>
+            </ul>
+
+            <section class="bg-white rounded-b-lg">
+                <ul class="flex justify-between list-reset p-3 border-t border-smoke">
+                    <li class="w-1/5 text-black-lightest">{{ number_format($project->debtScore(), 2) }}</li>
+
+                    <li class="w-1/5 text-black-lightest">{{ $project->oldPrs()->count() }}</li>
+
+                    <li class="w-1/5 text-black-lightest">{{ $project->oldIssues()->count() }}</li>
+
+                    <li class="w-1/5 text-black-lightest">{{ $project->prs()->count() }}</li>
+
+                    <li class="w-1/5 text-black-lightest">{{ $project->issues()->count() }}</li>
+                </ul>
+            </section>
+        </div>
+    </div>
+
+    {{--Pull Requests--}}
+    <div class="flex items-start border-b border-clouds">
         <h3 class="w-1/3 text-lg text-black-lightest py-6 font-thin">Pull Requests</h3>
 
         <ul class="w-2/3">
@@ -27,9 +62,10 @@
                 <x-pull-request :pr-data="$pr" :project="$project" />
             @endforeach
         </ul>
-    </section>
+    </div>
 
-    <section class="flex items-start border-b-1 border-clouds">
+    {{--Issues--}}
+    <div class="flex items-start border-b-1 border-clouds">
         <h3 class="w-1/3 text-lg text-black-lightest py-6 font-thin">Issues</h3>
 
         <ul class="w-2/3 list-reset">
@@ -37,9 +73,9 @@
                 <x-issue :issue="$issue" :project="$project" />
             @endforeach
         </ul>
-    </section>
+    </div>
 
-    <section class="flex flex-col items-end mt-6">
+    <div class="flex flex-col items-end mt-6">
         <a class="text-indigo no-underline" href="#">&#8593; Back to top</a>
-    </section>
-</div>
+    </div>
+</section>
