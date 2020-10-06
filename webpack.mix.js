14ecd5e0e564340649fe37a9fec556031db88591
Jon Sugar <jonathansugar@mac.com>
2020-10-06 14:07:04 +0100

    build: :package: bundle markdown styles into vendor.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,8 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js');
-mix.copy('resources/css/markdown.css', 'public/css');
 mix.postCss('resources/css/main.css', 'public/css', [
     tailwindcss('tailwind.js'),
 ]);
-
+mix.combine(['resources/css/markdown.css'], 'public/css/vendor.css');
 mix.browserSync('http://ozzie.test:8000');
