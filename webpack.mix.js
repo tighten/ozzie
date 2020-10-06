78ce00ef6408616c496dcc3be2a475c9ffa5da10
Jon Sugar <jonathansugar@mac.com>
2020-10-06 10:51:31 +0100

    build: :arrow_up: Upgrade node dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,9 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/app.js', 'public/js');
-
+mix.copy('resources/css/markdown.css', 'public/css');
 mix.postCss('resources/css/main.css', 'public/css', [
     tailwindcss('tailwind.js'),
 ]);
 
-mix.browserSync('https://ozzie.test');
+mix.browserSync('http://ozzie.test:8000');
