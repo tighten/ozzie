20bee8d736a44e491c8006a5bdce7959f7c07273
Jon Sugar <jonathansugar@mac.com>
2021-02-08 12:29:57 +0000

    style: 🎨 beautify code

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,41 +24,47 @@ module.exports = {
     'global-require': 'off',
     'import/extensions': 'off',
     'object-shorthand': 'off',
-    'max-len': ['error', {
-      code: 120,
-      ignoreComments: true,
-      ignoreTrailingComments: true,
-      ignoreUrls: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: false,
-      ignoreRegExpLiterals: false,
-      ignorePattern: 'class=".+"',
-    }],
+    'max-len': [
+      'error', {
+        code: 120,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: false,
+        ignoreRegExpLiterals: false,
+        ignorePattern: 'class=".+"',
+      },
+    ],
 
     // TypeScript -------------------------------------------------------------
 
-    // 'no-shadow': 'off',
-    // '@typescript-eslint/no-shadow': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
 
-    // 'no-unused-vars': 'off',
-    // '@typescript-eslint/no-unused-vars-experimental': 'error',
-    // '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars-experimental': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
 
-    // 'no-empty-function': 'off',
-    // '@typescript-eslint/no-empty-function': ['error', { allow: ['private-constructors'] }],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': [
+      'error', {
+        allow: ['private-constructors'],
+      },
+    ],
 
-    // 'no-redeclare': 'off',
-    // '@typescript-eslint/no-redeclare': 'error',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'error',
 
     // Classes ----------------------------------------------------------------
 
-    // 'class-methods-use-this': 'off',
-    // 'no-useless-constructor': 'off',
-    // '@typescript-eslint/no-useless-constructor': 'error',
+    'class-methods-use-this': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
 
-    // 'max-classes-per-file': 'warn',
-    // 'lines-between-class-members': 'off',
-    // 'no-underscore-dangle': 'off',
+    'max-classes-per-file': 'warn',
+    'lines-between-class-members': 'off',
+    'no-underscore-dangle': 'off',
 
     // Vue 3 ------------------------------------------------------------------
 
@@ -90,5 +96,3 @@ module.exports = {
     },
   },
 };
-
-/* eslint-disable @typescript-eslint/no-var-requires */
