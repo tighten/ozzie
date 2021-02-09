df5b0afc0e510e61a69b5664c89e9e428adb78bc
Jon Sugar <jonathansugar@mac.com>
2021-02-09 10:51:25 +0000

    feat: ✨ link to home from the Ozzie logo in the header

--- resources/ts/Pages/Layout.vue
+++ resources/ts/Pages/Layout.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div class="bg-white border-t-4 border-indigo relative z-10 shadow">
+    <div class="bg-white border-t-4 border-indigo relative z-10">
       <section class="max-w-4xl mx-auto px-2">
         <div class="flex justify-between items-center">
-          <h1 class="flex items-center">
-            <span class="font-light text-5xl text-indigo">O</span>
-            <span class="uppercase text-2xl leading-normal text-black-light font-semibold font-sans tracking-wide">zzie</span>
-          </h1>
+          <InertiaLink
+            href="/inertia"
+            method="get"
+          >
+            <h1 class="flex items-center">
+              <span class="font-light text-5xl text-indigo">O</span>
+              <span class="uppercase text-2xl leading-normal text-black-light font-semibold font-sans tracking-wide">zzie</span>
+            </h1>
+          </InertiaLink>
           <p class="font-sans italic font-normal leading-normal tracking-tight text-grey-blue-darkest">
             Addressing our open source debt
           </p>
