fd9d5d4c176312620118ba61cd8e28fbc65b4dd4
Jon Sugar <jonathansugar@mac.com>
2021-02-11 10:55:21 +0000

    style: 💄 improve readability

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,24 +2,17 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 const webpack = require('webpack');
 
-mix.ts('resources/ts/app.ts', 'public/js').vue({ version: 3, options: {} });
-mix.webpackConfig({
-    output: {
-        chunkFilename: 'js/[name].js?id=[chunkhash]',
-    },
-    plugins: [
-        new webpack.DefinePlugin(
-            {
-                __VUE_OPTIONS_API__: true,
-                __VUE_PROD_DEVTOOLS__: false,
-            },
-        ),
-    ],
-});
-mix.version();
-mix.sourceMaps();
-mix.postCss('resources/css/main.css', 'public/css', [
-    tailwindcss('tailwind.js'),
-]);
-mix.combine(['resources/css/markdown.css'], 'public/css/vendor.css');
-mix.browserSync('http://127.0.0.1:8000/inertia');
+const webpackConfig = {
+    output: { chunkFilename: 'js/[name].js?id=[chunkhash]' },
+    plugins: [new webpack.DefinePlugin({ __VUE_OPTIONS_API__: true, __VUE_PROD_DEVTOOLS__: false })],
+};
+
+mix.ts('resources/ts/app.ts', 'public/js').vue({ version: 3, options: {} })
+    .webpackConfig(webpackConfig)
+    .version()
+    .sourceMaps(false)
+    .postCss('resources/css/main.css', 'public/css', [
+        tailwindcss('tailwind.js'),
+    ])
+    .combine(['resources/css/markdown.css'], 'public/css/vendor.css')
+    .browserSync('http://127.0.0.1:8000/inertia');
