2ee288396faa24da1cb1b4a4303add391c334e59
Matt Stauffer <matt@tighten.co>
2021-03-08 18:06:47 -0500

    Drop un-used request

---
+++ app/Http/Controllers/Api/ProjectsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProjectResource;
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        return ProjectResource::collection(Project::all());
+    }
+
+    public function show(string $vendor, string $name)
+    {
+        // @todo Re-work using explicit route model binding
+        $project = Project::forPackagist($vendor, $name)->firstOrFail();
+
+        return new ProjectResource($project);
+    }
+}
