58007a7d33104062f9ba1f8c88e03dc2ee6d0564
Jon Sugar <jonathansugar@mac.com>
2021-08-17 17:29:48 +0100

    feat: update to the newest inertiajs version
    addresses https://github.com/tighten/ozzie/pull/93/files?file-filters%5B%5D=.css&file-filters%5B%5D=.dist&file-filters%5B%5D=.example&file-filters%5B%5D=.js&file-filters%5B%5D=.json&file-filters%5B%5D=.lock&file-filters%5B%5D=.ts&file-filters%5B%5D=.txt&file-filters%5B%5D=.vue&file-filters%5B%5D=dotfile#r681831693

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,7 +1,5 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { createApp, h } from 'vue';
-// @ts-ignore
-import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { DateTime } from 'luxon';
 import axios from 'axios';
@@ -15,16 +13,20 @@ InertiaProgress.init({
     showSpinner: true,
 });
 
-createInertiaApp({
-    resolve: (name) => import(`./Pages/${name}`),
-    setup({
-        el, app, props, plugin,
-    }) {
-        const ozzie = createApp({ render: () => h(app, props) });
-        ozzie.config.globalProperties.$luxon = DateTime;
-        ozzie.config.globalProperties.$http = axios;
-        ozzie.config.globalProperties.$route = route;
-        ozzie.use(plugin);
-        ozzie.mount(el);
+createInertiaApp(
+    {
+        resolve: (name) => import(`./Pages/${name}`),
+        setup({
+            el, app, props, plugin,
+        }) {
+            const ozzie = createApp({ render: () => h(app, props) });
+            ozzie.config.globalProperties.$luxon = DateTime;
+            ozzie.config.globalProperties.$http = axios;
+            ozzie.config.globalProperties.$route = route;
+            ozzie.component('InertiaHead', Head);
+            ozzie.component('InertiaLink', Link);
+            ozzie.use(plugin);
+            ozzie.mount(el);
+        },
     },
-});
+);
