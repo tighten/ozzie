b3f565545266861d4782284db230d5d6e4b1e420
Matt Stauffer <matt@tighten.co>
2021-08-17 10:39:35 -0400

    Add Bugsnag

--- config/logging.php
+++ config/logging.php
@@ -37,8 +37,12 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['daily', 'bugsnag'],
             'ignore_exceptions' => false,
+        ],
+
+        'bugsnag' => [
+            'driver' => 'bugsnag',
         ],
 
         'single' => [
