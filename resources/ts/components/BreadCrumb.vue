111aab0517b7ed6356b486861a1d2e6429255083
Andy Newhouse <hi@andymnewhouse.me>
2022-03-25 17:33:37 -0500

    Add a proper nav for breadcrumbs

--- resources/ts/components/BreadCrumb.vue
+++ resources/ts/components/BreadCrumb.vue
@@ -1,41 +1,67 @@
 <template>
-    <div class="mt-2 flex">
-        <InertiaLink
-            class="text-indigo-600"
-            :href="$route('projects.index')"
-            method="get">
-            Home
-        </InertiaLink>
-        <span
-            v-if="project"
-            class="mx-1">&gt;</span>
-        <InertiaLink
-            v-if="project"
-            class="text-indigo-600"
-            :href="ozzieProjectUrl(project.packagist_name)"
-            method="get">
-            {{ project.packagist_name }}
-        </InertiaLink>
-        <span
-            v-if="issue"
-            class="mx-1">&gt;</span>
-        <span v-if="issue">#{{ issue.number }}</span>
-    </div>
+    <nav class="flex mt-2" aria-label="Breadcrumb">
+        <ol role="list" class="flex items-center space-x-2">
+            <li>
+                <div>
+                    <InertiaLink
+                        :href="$route('projects.index')"
+                        class="text-gray-500 hover:text-indigo-600"
+                    >
+                        <HomeIcon
+                            class="flex-shrink-0 h-5 w-5"
+                            aria-hidden="true"
+                        />
+                        <span class="sr-only">Home</span>
+                    </InertiaLink>
+                </div>
+            </li>
+            <li v-if="project">
+                <div class="flex items-center">
+                    <ChevronRightIcon
+                        class="flex-shrink-0 h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                    />
+                    <InertiaLink
+                        :href="ozzieProjectUrl(project.packagist_name)"
+                        class="ml-2 text-sm font-medium text-gray-500 hover:text-indigo-600"
+                    >
+                        {{ project.packagist_name }}
+                    </InertiaLink>
+                </div>
+            </li>
+            <li v-if="issue">
+                <div class="flex items-center">
+                    <ChevronRightIcon
+                        class="flex-shrink-0 h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                    />
+                    <span class="ml-2 text-sm font-medium text-gray-500">
+                        #{{ issue.number }}
+                    </span>
+                </div>
+            </li>
+        </ol>
+    </nav>
 </template>
 <script lang="ts">
 import { PropType } from 'vue';
 import { Issue, Project, PullRequest } from '@/ozzie';
 import { ozzieProjectUrl } from '@/functions/url';
+import { ChevronRightIcon, HomeIcon } from '@heroicons/vue/solid';
 
 export default {
     name: 'BreadCrumb',
+    components: {
+        ChevronRightIcon,
+        HomeIcon,
+    },
     props: {
         project: {
             type: Object as PropType<Project>,
             default: null,
         },
         issue: {
-            type: Object as PropType<Issue|PullRequest>,
+            type: Object as PropType<Issue | PullRequest>,
             default: null,
         },
     },
