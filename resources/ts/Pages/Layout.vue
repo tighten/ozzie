426a9c803a097a02147d32e3477113baf41a3f51
Andy Newhouse <hi@andymnewhouse.me>
2022-03-28 22:19:14 -0500

    Add dark mode to layout

--- resources/ts/Pages/Layout.vue
+++ resources/ts/Pages/Layout.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
-        <div class="relative z-10 border-t-4 border-indigo-600 bg-white shadow">
-            <section class="mx-auto max-w-4xl px-2">
+        <div class="relative z-10 bg-white border-t-4 border-indigo-600 shadow dark:border-b dark:border-b-gray-700 dark:border-t-indigo-600 dark:bg-gray-800">
+            <section class="max-w-4xl px-2 mx-auto">
                 <div class="flex items-center justify-between h-16">
                     <InertiaLink
                         :href="$route('projects.index')"
                         method="get">
                         <h1 class="flex items-center">
-                            <span class="text-5xl font-light text-indigo-600">O</span>
-                            <span class="font-sans text-2xl font-semibold uppercase leading-normal tracking-wide text-black-light">zzie</span>
+                            <span class="text-5xl font-light text-indigo-600 dark:text-indigo-400">O</span>
+                            <span class="font-sans text-2xl font-semibold leading-normal tracking-wide text-gray-900 uppercase dark:text-gray-200">zzie</span>
                         </h1>
                     </InertiaLink>
-                    <p class="font-sans font-normal italic leading-normal tracking-tight text-gray-blue-darkest">
+                    <p class="font-sans italic font-normal leading-normal tracking-tight text-gray-700 dark:text-gray-400">
                         Addressing our open source debt
                     </p>
                 </div>
             </section>
         </div>
-        <div class="relative z-0 overflow-x-auto bg-frost font-sans">
-            <div class="mx-auto max-w-4xl px-2 pb-6">
+        <div class="relative z-0 overflow-x-auto font-sans bg-gray-100 dark:bg-gray-900">
+            <div class="max-w-4xl px-2 pb-6 mx-auto">
                 <slot />
             </div>
         </div>
