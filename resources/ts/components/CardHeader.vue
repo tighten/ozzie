15466790767cf26179a3cbdf0a5e77f40d684de1
Andy Newhouse <hi@andymnewhouse.me>
2022-04-01 13:54:38 -0500

    Adjust card header background, sizing and postition

--- resources/ts/components/CardHeader.vue
+++ resources/ts/components/CardHeader.vue
@@ -1,6 +1,6 @@
 <template>
-    <span class="absolute px-2 py-1 text-sm font-semibold leading-none text-gray-900 rounded-md shadow -top-6 dark:border dark:border-gray-700 bg-gray-50 dark:bg-gray-850 dark:text-gray-200">
-        <span class="table-cell p-4">
+    <span class="absolute px-2 py-1 text-sm font-semibold leading-none text-gray-900 bg-white rounded-md shadow -top-5 dark:border dark:border-gray-700 dark:bg-gray-850 dark:text-gray-200">
+        <span class="block px-2 py-2">
             <slot />
         </span>
     </span>
