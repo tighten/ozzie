3b85f8347c89b7603607607fd7e80096908f72b9
Andy Newhouse <hi@andymnewhouse.me>
2022-04-01 13:54:11 -0500

    Alter card padding

--- resources/ts/components/Card.vue
+++ resources/ts/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative bg-white rounded-lg shadow dark:bg-gray-800 dark:border dark:border-gray-700">
-        <div class="px-4 py-5">
+        <div class="p-4">
             <slot />
         </div>
     </div>
