9355a458b509fe8e1bcacb91320276c0c8e3fb1b
Shift <shift@laravelshift.com>
2023-05-25 23:11:39 +0000

    Remove redundant typing from DocBlocks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,8 +19,6 @@ class Kernel extends ConsoleKernel
 
     /**
      * Define the application's command schedule.
-     *
-     * @return void
      */
     protected function schedule(Schedule $schedule): void
     {
@@ -41,8 +39,6 @@ class Kernel extends ConsoleKernel
 
     /**
      * Register the commands for the application.
-     *
-     * @return void
      */
     protected function commands(): void
     {
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -42,8 +42,6 @@ class RegisterController extends Controller
 
     /**
      * Get a validator for an incoming registration request.
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data): \Illuminate\Contracts\Validation\Validator
     {
@@ -56,8 +54,6 @@ class RegisterController extends Controller
 
     /**
      * Create a new user instance after a valid registration.
-     *
-     * @return \App\User
      */
     protected function create(array $data): User
     {
