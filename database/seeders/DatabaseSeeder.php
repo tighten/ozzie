bcca3c8a2b79ebe31b503a852804a4ca42f945c2
vaughnjones-marcus <vaughnjones.marcus@gmail.com>
2023-11-13 09:08:08 -0600

    Move seed files to database/seeders, add namespace

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
