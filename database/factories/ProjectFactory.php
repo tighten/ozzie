3abca471c1c74b73e841f60e474dee8abaa0b0c1
Guillermo Cava <gcavanunez@gmail.com>
2024-01-26 18:13:12 -0500

    public routes tests

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\GitHub\Dto\Issue;
+use App\GitHub\Dto\PullRequest;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ProjectFactory extends Factory
@@ -15,8 +17,8 @@ class ProjectFactory extends Factory
             'namespace' => config('app.organization'),
             'packagist_name' => config('app.organization') . '/' . $name,
             'is_hidden' => false,
-            'issues_count' => fake()->numberBetween(0, 5),
-            'pull_requests_count' => fake()->numberBetween(0, 5),
+            'issues_count' => 0,
+            'pull_requests_count' => 0,
             'issues' => [],
             'pull_requests' => [],
         ];
@@ -27,6 +29,55 @@ class ProjectFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'is_hidden' => true,
+            ];
+        });
+    }
+
+    public function withIssues(array $issues = [['id' => 1]]): Factory
+    {
+        return $this->state(function (array $attributes) use ($issues) {
+            return [
+                'issues_count' => count($issues),
+                'issues' => collect($issues)->map(function ($issue) {
+                    return new Issue([
+                        'created_at' => now(),
+                        'html_url' => fake()->url(),
+                        'pull_request' => null,
+                        'title' => fake()->sentence(),
+                        'number' => $issue['id'],
+                        'body' => fake()->paragraph(),
+                        'labels' => [],
+                        'user' => [
+                            'html_url' => fake()->url(),
+                            'login' => fake()->userName(),
+                        ],
+                    ]);
+                })->toArray(),
+            ];
+        });
+    }
+
+    public function withPrs(array $pullRequests = [[]]): Factory
+    {
+        return $this->state(function (array $attributes) use ($pullRequests) {
+            return [
+                'pull_requests_count' => count($pullRequests),
+                'pull_requests' => collect($pullRequests)->map(function ($pullRequest, $index) {
+                    return new PullRequest(array_merge([
+                        'created_at' => now(),
+                        'html_url' => fake()->url(),
+                        'draft' => fake()->boolean(),
+                        'title' => fake()->sentence(),
+                        'number' => $index,
+                        'body' => fake()->paragraph(),
+                        'node_id' => fake()->uuid(),
+                        'labels' => [],
+                        'user' => [
+                            'html_url' => fake()->url(),
+                            'login' => fake()->userName(),
+                        ],
+                    ], $pullRequest));
+                })->toArray(),
             ];
         });
     }
