f8f190cd726afdfc6f9e8ba9e4d9635b58e92b2d
Matt Stauffer <matt@tighten.co>
2026-02-21 16:04:48 -0500

    Update Packagist "find package" search to handle pagination

--- app/Remotes/Packagist/Search.php
+++ app/Remotes/Packagist/Search.php
@@ -37,13 +37,21 @@ class Search
     protected function findPackage()
     {
         $packages = Cache::remember("packagist-search::{$this->namespace}-{$this->name}", config('cache.timeout'), function () {
-            $response = Http::get($this->url);
+            $allResults = collect();
+            $url = $this->url;
 
-            if ($response->ok()) {
-                return $response->collect('results');
+            while ($url) {
+                $response = Http::get($url);
+
+                if (! $response->ok()) {
+                    break;
+                }
+
+                $allResults = $allResults->merge($response->collect('results'));
+                $url = $response->json('next');
             }
 
-            return collect();
+            return $allResults;
         });
 
         // Loop over packages and find the one with the matching repo
