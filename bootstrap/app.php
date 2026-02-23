bfbb357fe5f3d621aa71b93aefe9321e2bc1cd8e
Shift <shift@laravelshift.com>
2026-02-23 22:05:09 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/home');
+
+        $middleware->web(\App\Http\Middleware\HandleInertiaRequests::class);
+
+        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
