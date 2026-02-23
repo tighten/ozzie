6c357cbd40be93e3c5ae6bbfdb617b90e781525e
Shift <shift@laravelshift.com>
2026-02-23 22:05:09 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        (env('APP_ENV', 'production') != 'testing' ? App\Providers\NovaServiceProvider::class : null),
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
--- config/app.php
+++ config/app.php
@@ -9,17 +9,6 @@ return [
 
     'twitter' => env('APP_TWITTER', 'tightenco'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge(array_filter([
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        (env('APP_ENV', 'production') != 'testing' ? App\Providers\NovaServiceProvider::class : null),
-        App\Providers\RouteServiceProvider::class,
-    ]))->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
