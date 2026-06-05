a12f9ed57f302a937182a3f9f0547e1265f4c515
Nico Devs <nico1485@gmail.com>
2026-06-05 11:44:09 -0300

    Revert concat_space changes incompatible with CI duster version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         (env('APP_ENV', 'production') != 'testing' ? NovaServiceProvider::class : null),
     ])
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
